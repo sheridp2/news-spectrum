aad06753545881dac4034ddaafb3e0ff7329341e
Patrick Sheriddan <sheridp2@gmail.com>
2021-02-01 13:23:00 -0800

    inital commit, boilerplate added

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import PokemonList from "./PokemonList";
+
+function App() {
+  return (
+    <div>
+      <PokemonList />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/PokemonList.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function PokemonList() {
+  return <div>PokeList</div>;
+}
+
+export default PokemonList;
